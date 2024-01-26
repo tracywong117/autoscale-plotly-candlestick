5d2d0481793d9988fe12d97b62ebd1e6f4d0579c
tracywong117 <tracywong117@gmail.com>
2024-01-26 17:02:56 +0800

    initial commit

--- autoscale-candlestick.py
+++ autoscale-candlestick.py
@@ -52,44 +52,44 @@ def find_min_max(df, start_date, end_date):
     return min_values, max_values
 
 
-# @app.callback(Output("stock-chart", "figure"), [Input("stock-chart", "relayoutData")])
-# def display_relayout_data(relayoutData):
-#     global stockfig
-#     start = end = 0
+@app.callback(Output("stock-chart", "figure"), [Input("stock-chart", "relayoutData")])
+def display_relayout_data(relayoutData):
+    global stockfig
+    start = end = 0
 
-#     if not relayoutData:
-#         raise PreventUpdate
+    if not relayoutData:
+        raise PreventUpdate
 
-#     if relayoutData.get("xaxis.range", False):
-#         start = relayoutData["xaxis.range"][0]
-#         end = relayoutData["xaxis.range"][1]
-#     elif relayoutData.get("xaxis.range[0]", False):
-#         start = relayoutData["xaxis.range[0]"]
-#         end = relayoutData["xaxis.range[1]"]
+    if relayoutData.get("xaxis.range", False):
+        start = relayoutData["xaxis.range"][0]
+        end = relayoutData["xaxis.range"][1]
+    elif relayoutData.get("xaxis.range[0]", False):
+        start = relayoutData["xaxis.range[0]"]
+        end = relayoutData["xaxis.range[1]"]
 
-#     if start != 0:
-#         print("Start: ", start.split(" ")[0])
-#         print("End: ", end.split(" ")[0])
-#         start = start.split(" ")[0]
-#         end = end.split(" ")[0]
+    if start != 0:
+        print("Start: ", start.split(" ")[0])
+        print("End: ", end.split(" ")[0])
+        start = start.split(" ")[0]
+        end = end.split(" ")[0]
 
-#         ymin, ymax = find_min_max(df, start, end)
+        ymin, ymax = find_min_max(df, start, end)
 
-#         diff = ymax - ymin
+        diff = ymax - ymin
 
-#         ymin -= min(diff * 0.2, 2)
-#         ymax += min(diff * 0.2, 2)
+        ymin -= min(diff * 0.2, 2)
+        ymax += min(diff * 0.2, 2)
 
-#         stockfig.update_layout(
-#             xaxis_autorange=False,
-#             yaxis_autorange=False,
-#             xaxis_range=[start, end],
-#             yaxis_range=[ymin, ymax],
-#         )
+        stockfig.update_layout(
+            xaxis_autorange=False,
+            yaxis_autorange=False,
+            xaxis_range=[start, end],
+            yaxis_range=[ymin, ymax],
+        )
 
-#         return stockfig
-#     else:
-#         raise PreventUpdate
+        return stockfig
+    else:
+        raise PreventUpdate
 
 
 # Run the app
